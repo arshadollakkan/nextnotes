f474b59a4945945cbbad733518eb844a5d0d7c8c
arsh <ollakkanarshad@gmail.com>
2024-07-26 12:00:30 +0530

    clinet/server pages in dynamically

--- src/app/about/product/[subproduct]/page.js
+++ src/app/about/product/[subproduct]/page.js
@@ -1,7 +1,17 @@
+  
+
+
+
+
+  //client side componentdynamic routing of page(page oru pad ubayogikkugha)
+  "use client"
+  import { useParams } from "next/navigation";
 const Subproduct = () => {
+  let par=useParams()  
+   
     return ( 
       <div>
-          <h1>this is sub product</h1>
+          <h1>page number is{par.subproduct}</h1>
           <h6>price is 500</h6>
       </div>
         
